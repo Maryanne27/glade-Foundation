f16fc4548675efd919b42379d4294e43c5ca75ba
maryanne <j.ryanne20@gmail.com>
2023-03-01 10:46:44 +0100

    added overflow hiddem and margin auto

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -8,15 +8,15 @@ function Info() {
     <div className='info'>
        <div className="container">
          <div className="row">
-           <div className="col-lg-4 card2">
+           {/* <div className="col-lg-4 card2">
   <img src={img2} alt="covid19" height='400px' width='350px' />
   <div className="intro">
       <h1>GLADE FOUNDATION GOAL</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt, sunt amet dolorum saepe aliquam ipsa at ratione doloremque eius dicta deleniti unde aperiam. Molestias iste quaerat cupiditate vel explicabo aperiam.</p>
   </div>
-           </div>
+           </div> */}
 
-<div className="col-lg-4 card2 green">
+<div className="col-md-6 card2 green">
 <img src={vaccine} alt="vaccineimage" height='400px' width='350px'/>
 <div className="intro">
       <h1>COVID-19 VACCINATION</h1>
@@ -25,7 +25,7 @@ function Info() {
 </div>
 
 
-<div className="col-lg-4 card2">
+<div className="col-md-6 card2">
  <img src={img1} alt="donationimage" height='400px' width='350px'/>
  <div className="intro green">
       <h1>DONATIONS ARE STILL NEEDED</h1>
